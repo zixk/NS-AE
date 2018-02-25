113a72af14cdee0483b0b22e5b0a629ca0eec415
root <root@localhost.localdomain>
2018-02-25 17:58:19 +0000

    Working but funky

--- trace.sh
+++ trace.sh
@@ -3,7 +3,21 @@
 correct_trace(){
     while read -r line
     do 
-        echo  print "$line"
+        number="$(cut -d" " -f1 <<<"$line")"
+        routerAddr="$(cut -d" " -f3 <<<"$line")"
+        if [ "$number" = 1 ] ; then 
+            fromIP="$(cut -d" " -f2 <<<"$line")"
+            echo AM NUMBA JUAN
+            FIRST="\"$routerAddr\""
+        elif [ "$number" = "traceroute" ] ; then
+            echo AM TRACE
+        else
+            if [ "$routerAddr" != "*" ] ; then
+                SECOND="\"$routerAddr\""
+                echo "$FIRST" -- "$SECOND" >> "./youtube4.dot"
+                FIRST="$SECOND"
+            fi
+        fi
     done <<< "$OUTPUT"
 }
 
@@ -13,15 +27,13 @@ fi
 if [ $1 -eq "-6" ] ; then
     IP="IPv6"
 fi
-echo 'IP --> '$IP
 
-inputfile="./test.out"
+inputfile="../dnslu/youtube.out"
 while IFS= read -r line 
 do
     ipType="$(cut -d" " -f2-2 <<<"$line")"
     if [ "$ipType" = "$IP" ] ; then
         ipAddress="$(cut -d" " -f3 <<<"$line")"
-        echo "$ipAddress"
         if [ "$IP" = "IPv4" ] ; then
             OUTPUT="$(traceroute -4 -q 1 -n "$ipAddress")"
             correct_trace
